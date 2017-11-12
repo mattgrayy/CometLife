9d8bef2ac232c2ac53a287047bbf091c13cf9091
mattgrayy <matt2.gray@live.uwe.ac.uk>
2017-11-12 15:35:09 +0000

    Basic scene setup

---
+++ CometLife_UnityProj/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
